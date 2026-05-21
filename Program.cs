08072c029bea6d5d4e1ab41277e93bb2a8e17299
HUMBERTO <hrruiz@uamv.edu.ni>
2026-05-20 23:08:06 -0600

    Todo el trabajo

--- Program.cs
+++ Program.cs
@@ -12,44 +12,57 @@ int[] notas = new int[25];
 
 //Agregar
 
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Ingrese las notas de los 25 estudiantes:");
+Console.ResetColor();
+for (int i = 0; i < 25; i++)
+{
+    try
+    {
+        Console.Write($"Nota #{i + 1}: ");
+        notas[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Valor invalido. Por favor ingrese un numero entero.");
+        Console.ResetColor();
+        i--;
+    }
+}
 //Mostrar
 Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("=== MOSTRANDO NOTAS ===");
+Console.WriteLine("=== NOTAS AGREGADAS ===");
 Console.ResetColor();
 for (int i = 0; i < 25; i++)
 {
     Console.ForegroundColor = ConsoleColor.Blue;
     Console.WriteLine($"Nota {i + 1}: {notas[i]}");
     Console.ResetColor();
-    if (notas[i] < 70)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-    }
+    if (notas[i] < 70) Console.ForegroundColor = ConsoleColor.Red;
+    else Console.ForegroundColor = ConsoleColor.Green;
     Console.ResetColor();
 }
 
 //3 primeros lugares
-Array.Sort(notas[]);
-Array.Reverse(notas[]);
 
-Console.WriteLine("========= NOTAS TOP 3 ========")
+
+Console.WriteLine("========= NOTAS TOP 3 ========");
 for (int i = 0; i < 3; i++)
 {
+    Array.Sort(notas);
+    Array.Reverse(notas);
     Console.WriteLine($"{i + 1}. {notas[i]}");
 }
 //Mostrar descendente
 
 Console.ForegroundColor = ConsoleColor.Magenta;
-Console.WriteLine("=== NOTAS ===");
+Console.WriteLine("=== NOTAS DESCENDENTES ===");
 Console.ResetColor();
 for (int i = 0; i < 25; i++)
 {
-    Array.Sort(notas[]);
-    Array.Reverse(notas[]);
+    Array.Sort(notas);
+    Array.Reverse(notas);
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine($"Nota {i + 1}: {notas[i]}");
     Console.ResetColor();
--- obj/Debug/net10.0/RegistroNotas.AssemblyInfo.cs
+++ obj/Debug/net10.0/RegistroNotas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RegistroNotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c032eb1dde9fd1461f33cda5436b6d4b619fef45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b0f88df623fac4656df05ffbabffe8b1d101f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RegistroNotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RegistroNotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
